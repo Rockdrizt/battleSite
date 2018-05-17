1d7982c0e60b467171f0a2e9a4bede2053606d9b
DiegoFRM <diego@yogome.com>
2018-05-16 19:23:21 -0500

    update css

--- yogoTournament/mathServer/mobile/assets/js/jquery.easy_slides.js
+++ yogoTournament/mathServer/mobile/assets/js/jquery.easy_slides.js
@@ -24,12 +24,12 @@ Usage:
   $.fn.EasySlides = function( options ) {  
     var settings = $.extend( {
       'autoplay': false,
-      'timeout': 3000,
+      'timeout': 2000,
       'show': 5,
       'vertical': false,
       'reverse': false,
       'touchevents': true,
-      'delayaftershow': 300,
+      'delayaftershow': 100,
       'stepbystep': true,
       'startslide': 0,
       'distancetochange': 10,
