3691cbe62bdab9fea9abdf219cc4df8f682baf7f
Pawel Lewinski <pawelranger@hotmail.com>
2018-05-24 18:34:49 -0500

    take out inGame div, who was causing problems in old devices

--- yogoTournament/mathClient/js/main.js
+++ yogoTournament/mathClient/js/main.js
@@ -1,7 +1,7 @@
 window.minigame = window.minigame || {}
 
 function startGame(){
-	window.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, "inGame", {init: init, create: create }, true, true);
+	window.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, null, {init: init, create: create }, true, true);
     document.body.style.visibility = "hidden"
 
 	function preloadScenes(sceneList){
