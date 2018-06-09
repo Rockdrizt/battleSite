aee08c2fe0b75f726bbcb82d3b3799315de32a4f
Pawel Lewinski <pawelranger@hotmail.com>
2018-06-08 20:37:47 -0500

    load particles in spine

--- playground/js/libs/spineLoader.js
+++ playground/js/libs/spineLoader.js
@@ -1,48 +1,56 @@
 var spineLoader = function () {
-	function getSpineEvents(cacheKey, currentLoader, currentScene) {
+
+	var currentLoader
+
+	function addSound(functionData, soundsList, assetsSounds){
+		var soundObj = {
+			name:functionData.param,
+			file:soundsList[functionData.param]
+		}
+
+		assetsSounds.push(soundObj)
+		currentLoader.audio(soundObj.name, soundObj.file);
+	}
+
+	function getSpineEvents(cacheKey, currentScene) {
 		var jsonFile = game.cache.getJSON(cacheKey)
 		console.log(cacheKey)
 		var events = jsonFile.events
 		// console.log(events, spine)
-		var soundsAdded = {}
+
 		var soundsList = game.cache.getJSON('sounds')
 		var assetsSounds = currentScene.assets.sounds
+
+		currentScene.assets.particles = currentScene.assets.particles || []
+		var assetsParticles = currentScene.assets.particles
 
 		for(var key in events){
 			// var event = events[key]
 			var functionData = getFunctionData(key)
 
 			if((functionData)&&(functionData.name === "PLAY")){
-				var soundObj = {
-					name:functionData.param,
-					file:soundsList[functionData.param]
-				}
-				if(!soundsAdded[soundObj.name]){
-					assetsSounds.push(soundObj)
-					currentLoader.audio(soundObj.name, soundObj.file);
-					soundsAdded[soundObj.name] = soundObj.name
-				}
+				addSound(functionData, soundsList, assetsSounds)
+			}
+
+			if((functionData)&&(functionData.name === "SPAWN")){
+				addParticle(functionData, assetsParticles)
 			}
 		}
 	}
 
 	function getFunctionData(value) {
-		var indexOfFunc = value.indexOf(":")
-		var functionName = null
-		var param = null
+		var functionArrays = value.split(":")
+		var functionName = functionArrays[0]
+		var params = functionArrays.slice(1)
 
-		if(indexOfFunc > -1){
-			functionName = value.substr(0, indexOfFunc)
-			param = value.substr(indexOfFunc + 1)
-		}
-		// console.log(functionName, param)
 
-		return {name: functionName, param: param}
+		return {name: functionName, params: params}
 	}
 
 	function loadSpine(loader, currentSpine, loadingFiles, currentScene) {
+		currentLoader = currentLoader
 		loader.spine(currentSpine.name, currentSpine.file, currentSpine.scales)
-		loadingFiles[currentSpine.name] = {onComplete:getSpineEvents.bind(null, currentSpine.name, loader, currentScene)}
+		loadingFiles[currentSpine.name] = {onComplete:getSpineEvents.bind(null, currentSpine.name, currentScene)}
 	}
 
 	return {
