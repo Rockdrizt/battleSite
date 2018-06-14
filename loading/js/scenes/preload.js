2f3a781d648b6cfb884704b5cbfd72ddb0662611
Rodrigo Escobar Gutierrez <rockdrizt@gmail.com>
2018-06-14 15:00:04 -0500

    loading screen, yogoSelector fixed

--- loading/js/scenes/preload.js
+++ loading/js/scenes/preload.js
@@ -6,10 +6,11 @@ var preloaderIntro = function(){
 				json: "images/preload/atlas.json",
 				image: "images/preload/atlas.png"
 			}],
-		images: [            
-            { 
+		images: [
+           { 
                 name:'logo',
-                file: "images/preload/bgTile.png"}
+                file: "images/preload/logo.png"}
+            
         ],
 		sounds: [
 
@@ -31,22 +32,8 @@ var preloaderIntro = function(){
 		create: function(event){
 
 			var sceneGroup = game.add.group()
-            
-            var bmd = game.add.bitmapData(game.world.width, game.world.height)
-            var back = bmd.addToWorld()
 
-            var y = 0
-
-            for (var i = 0; i < bmd.height; i++)
-            {
-                var color = Phaser.Color.interpolateColor(0x05072B, 0x0D014D, bmd.height, i)
-
-                bmd.rect(0, y, bmd.width, y + 1, Phaser.Color.getWebRGB(color))
-                y += 2
-            }
-            sceneGroup.add(back)
-
-			var logo = sceneGroup.create(game.world.centerX, game.world.centerY, 'logoAtlas', 'logo')
+			var logo = sceneGroup.create(game.world.centerX, game.world.centerY, 'logo')
 			logo.anchor.setTo(0.5, 0.5)
             logo.scale.setTo(0);
              game.add.tween(logo.scale).to({x:0.5,y:0.5},400, Phaser.Easing.Back.Out,true)
@@ -64,7 +51,7 @@ var preloaderIntro = function(){
 			loadingGroup.topBar = loadingTop
 
 			loadingGroup.x = game.world.centerX - loadingGroup.width * 0.5
-			loadingGroup.y = (game.world.centerY + 200) - loadingGroup.height * 0.5
+			loadingGroup.y = (game.world.centerY + 100) - loadingGroup.height * 0.5
 
 			loadingBar = loadingGroup
 			loadingBar.topBar.width = 0
