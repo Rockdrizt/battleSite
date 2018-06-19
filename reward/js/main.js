d00986fa052b9229bf694c2cfc41022e1e773c03
Dafne Bernardino <dafne.bernardino@yogome.com>
2018-06-19 15:56:50 -0500

    Comienzo de pantalla victoria

---
+++ reward/js/main.js
@@ -0,0 +1,62 @@
+window.minigame = window.minigame || {}
+
+function startGame(){
+	window.game = new Phaser.Game(document.body.clientWidth, document.body.clientHeight, Phaser.canvas, null, {init: init, create: create }, true, true);
+    document.body.style.visibility = "hidden"
+
+	function preloadScenes(sceneList){
+
+    	function onCompletePreloading(){
+
+			function onLoadFile(event){
+	    		var loaderScene = sceneloader.getScene("preloaderIntro")
+	    		loaderScene.updateLoadingBar(event.totalLoaded, event.totalFiles)
+	    	}
+
+	    	function onCompleteSceneLoading(){
+				sceneloader.show("reward")
+	    	}
+
+	      	sceneloader.preload(sceneList, {onLoadFile: onLoadFile, onComplete: onCompleteSceneLoading})
+            sceneloader.show("preloaderIntro")
+    	}
+
+        document.body.style.visibility = "visible"
+    	sceneloader.preload([preloaderIntro], {onComplete: onCompletePreloading})
+	}
+
+    function init(){
+
+        var fullWidth = 540
+        var fullHeight = 1080
+
+        var ratio = document.body.clientWidth / document.body.clientHeight
+        var gameHeight = Math.round(fullHeight)
+        var gameWidth = Math.round(fullHeight * ratio)
+
+        game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT
+        game.scale.setGameSize(gameWidth, gameHeight)
+
+        game.stage.backgroundColor = "#ffffff"
+        game.time.advancedTiming = true
+        game.stage.disableVisibilityChange = true;        
+
+        game.plugins.add(Fabrique.Plugins.Spine);
+        
+        var language = "EN"
+
+        localization.setLanguage(language)
+
+        window.minigame.game = window.game
+    	sceneloader.init(game)
+    	sound.init(game)
+    }
+
+    function create(){
+
+    	preloadScenes([
+            reward
+    	])
+    }
+}
+startGame()
