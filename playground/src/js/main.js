7e254e380099fbca23f051fb4ae3bb7944d4eb04
theLittleSeas <mares@yogome.com>
2018-07-16 12:35:55 -0500

    Fixed Dinamita Super and Ultra / Added New Particles for Dinamita

--- playground/src/js/main.js
+++ playground/src/js/main.js
@@ -80,7 +80,7 @@ function startGame(){
     	sound.init(game)
 
 		var teams = [
-			["yogotarDinamita", "yogotarEstrella", "yogotarLuna"],
+			["yogotarDinamita", "yogotarDinamita", "yogotarLuna"],
 		
 			["yogotarEagle", "yogotarDinamita", "yogotarEstrella"],
 		
