87d4d04bf652c7a960fd162110691a6744d05e5f
theLittleSeas <mares@yogome.com>
2018-07-16 18:13:12 -0500

    Added Luna Super Attack / Updated Earth Normal

--- playground/src/js/main.js
+++ playground/src/js/main.js
@@ -80,7 +80,7 @@ function startGame(){
     	sound.init(game)
 
 		var teams = [
-			["yogotarDinamita", "yogotarLuna", "yogotarLuna"],
+			["yogotarDinamita", "yogotarEstrella", "yogotarLuna"],
 		
 			["yogotarEagle", "yogotarDinamita", "yogotarEstrella"],
 		
