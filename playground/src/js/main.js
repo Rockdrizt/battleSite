3c7eb99e56b6dc87df27de6c83230f59359f1c51
theLittleSeas <mares@yogome.com>
2018-07-18 18:43:48 -0500

    Created Nao Data / Updated Dinamita / Fixed Ultra Arthurius

--- playground/src/js/main.js
+++ playground/src/js/main.js
@@ -83,7 +83,7 @@ function startGame(){
     	sound.init(game)
 
 		var teams = [
-			["yogotarEagle", "yogotarDinamita", "yogotarTomiko"],
+			["yogotarEagle", "yogotarNao", "yogotarTomiko"],
 			["yogotarEagle", "yogotarTomiko", "yogotarEstrella"],
 		
 		]
