01e4de22f1d3e8ea025fa81c84dd458dff9aa740
theLittleSeas <mares@yogome.com>
2018-07-18 16:13:34 -0500

    Added Tomiko Super and Ultra / Fixed Impact Wind Particles / Updated Super and Ultra Eagle

--- playground/src/js/main.js
+++ playground/src/js/main.js
@@ -83,7 +83,7 @@ function startGame(){
     	sound.init(game)
 
 		var teams = [
-			["yogotarDinamita", "yogotarEagle", "yogotarLuna"],
+			["yogotarDinamita", "yogotarTomiko", "yogotarLuna"],
 			["yogotarEagle", "yogotarTomiko", "yogotarEstrella"],
 		
 		]
