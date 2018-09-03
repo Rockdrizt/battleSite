fe12329e2c4d6a7283883bf1375e6a409de961a7
DiegoFRM <diego@yogome.com>
2018-09-03 18:54:38 -0500

    add delete question and add json questions

--- onboarding/js/custom_questions.js
+++ onboarding/js/custom_questions.js
@@ -1,32 +1,97 @@
-var countQuestions = 1;
+var countQuestions = 0;
 var optionLetters = ["a","b","c","d"];
+var _buttonAddQuestion = $(".questions__botton--add");
+var questionsArray = [];
+
+function addElementArray(number){
+    questionsArray[number-1] = new Array;
+    questionsArray[number-1].question = $("#questionText_" + number).val();
+    questionsArray[number-1].answer1 = $("#AnswerText_" + number + "_1").val();
+}
+function saveInfoinArray(){
+    var getTotalQuestions = $(".questions__container").children().length;
+    for(var i = 0;i<=getTotalQuestions-1;i++){
+        questionsArray[i].question = $("#questionText_" + [i+1]).val();
+        questionsArray[i].answer1 = $("#AnswerText_" + [i+1] + "_1").val();
+    }
+}
+
+
 
 function addQuestion(){
     countQuestions++
-    $(".questions__body").append(`
-        <hr>
+    var body = $(".questions__body");
+    $(".questions__container").append(`<section id= "question__new_`+ countQuestions +`" class="question__new"></section>`);
+    var container = $("#question__new_" + countQuestions)
+    container.append(`  
        <div class="questions__item">
                         <div class="questions__number"><span>`+countQuestions+`</span></div>
                         <div class="question__section--text">
-                            <div class="text_limit">0/130</div>
-                            <input  class="input--text questions__input" placeholder="Pregunta `+countQuestions+`">
+                            <div id="limitTextQuestion_`+countQuestions+`" class="text_limit" limit="130">130/130</div>
+                            <textarea id="questionText_`+ countQuestions +`" class="input--text questions__input" place="`+countQuestions+`" placeholder="Pregunta" wrap="soft" maxlength="130" spellcheck="true" rows="2" cols="2" ></textarea>
                         </div>
                         <div class="load_image--load"><img src="img/load_image.png"></div>
+                        <div class="load_image--load question__button--delete" index="`+countQuestions+`"><img src="img/crash_button.png"></div>
                     </div>`);
         for(var i = 1;i<=4;i++){
-            $(".questions__body").append(`
+            container.append(`
                 <div class="answers__item">
                             <div class="answer__letter">`+optionLetters[i-1]+`)</div>
                             <div class="question__section--text">
-                                <div class="text_limit__answer">0/70</div>
-                                <input   class="input--text answer__input" placeholder="respuesta `+optionLetters[i-1]+`">
+                                <div id="limitTextAnswer_`+countQuestions+`_`+i+`" class="text_limit__answer" limit="70">70/70</div>
+                                <textarea id="AnswerText_`+countQuestions+`_`+i+`" class="input--text answer__input" placeholder="respuesta `+optionLetters[i-1]+`" rows="1" cols="2" place="`+countQuestions+`" answerNumber="`+i+`" maxlength="70" spellcheck="true"></textarea>
                             </div>
-                            <div class="star__image--select">
+                            <div id="choiceCorrect_`+countQuestions+`_`+i+`" class="star__image--select answer__choice_`+countQuestions+`" index="`+countQuestions+`">
                                 <div class="circle--select"></div>
-                                <img src="img/circle_select.png">
+                                <img src="img/bg_select_answer.png">
                             </div>
                         </div> 
             `);
+            //choice correct
+            $( "#choiceCorrect_" + countQuestions + "_" + i  ).click(function(){
+                $(".answer__choice_" + $(this).attr("index")).find("img").attr("src","img/bg_select_answer.png")
+                $(this).find("img").attr("src","img/correct_select.png")
+            });
+            
+            //change text limit of answers
+            $( "#AnswerText_" + countQuestions + "_" + i  ).on('input',function(e){
+                var valueAnswer = $( this ).val().length;
+                var limitAnswerText = $("#limitTextAnswer_" + $( this ).attr("place") +"_"+ $( this ).attr("answerNumber") );
+                var restAnswerLimit =  parseInt(limitAnswerText.attr("limit")) - valueAnswer;
+                limitAnswerText.text( restAnswerLimit  +"/70")
+          })
+        }   
+    //delete question
+    $("#question__new_" + countQuestions).find(".question__button--delete").click(function(){
+        $("#question__new_" + $(this).attr("index")).remove();
+        countQuestions--
+        var getTotalQuestions = $(".questions__container").children().length;
+        var hijos = []
+        $('section','.questions__container').each(function(){
+           hijos.push($(this).attr("id"));
+        });
+        for(var p = 0;p<getTotalQuestions;p++){  
+            $("#" + hijos[p]).attr("id", "question__new_" + [p+1] );
+            var element = $(".questions__container").children()[p].id;
+            $("#" + element).find(".questions__number").find("span").html([p+1]);
+            $("#" + element).find(".question__button--delete").attr("index",[p+1]);
         }
-                     
+        //body.animate({scrollTop: (body.height() * $(this).attr("index"))},"2000"); 
+    });
+    //scroll animate 
+    body.animate({scrollTop: (body.height() * countQuestions)},"2000"); 
+    //change text limit of question
+    $( "#questionText_" + countQuestions ).on('input',function(e){
+        var value = $( this ).val().length;
+        var limitText = $("#limitTextQuestion_" + $( this ).attr("place"));
+        var restLimit =  parseInt(limitText.attr("limit")) - value;
+        limitText.text( restLimit  +"/130")
+  })  
+
+    addElementArray(countQuestions);
 }
+
+_buttonAddQuestion.click(function(){
+    addQuestion();
+    
+});
