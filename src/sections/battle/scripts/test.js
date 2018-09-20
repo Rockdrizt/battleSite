33d15f49945a82a5878450798870a23f651d20b2
theLittleSeas <mares@yogome.com>
2018-09-20 13:01:12 -0500

    Fixed Dinamita Wrong Smoke Particle Rotation

--- src/sections/battle/scripts/test.js
+++ src/sections/battle/scripts/test.js
@@ -85,7 +85,7 @@ function startGame(){
 		var defaultSkin = 2
 		var teams = [
 
-			["yogotarTheffanie", "yogotarNao", "yogotarDazzle"],
+			["yogotarTheffanie", "yogotarDinamita", "yogotarDazzle"],
 			["yogotarLuna", "yogotarDinamita", "yogotarDinamita"],
 
 		]
